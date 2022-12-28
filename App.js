11e4a0ca40615389464609a0703db722d614b777
Tatev-94 <117387297+Tatev-94@users.noreply.github.com>
2022-12-28 14:40:26 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './pages/Header/Header.module.css'
+import './App.css';
+import Header from './pages/Header/Header';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home />
+
+
+    </div>
+  );
+}
+
+export default App;
